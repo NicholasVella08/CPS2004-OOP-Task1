949eae3bdc6d752d964a11155e44df7703cf5e6b
NicholasVella08 <103609198+NicholasVella08@users.noreply.github.com>
2023-01-07 21:39:56 +0100

    creating the map class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+class Village {
+public:
+    int x;
+    int y;
+    int health;
+    int owner;
+    std::vector<int> resources;
+    std::vector<std::string> buildings;
+};
+
+
+class Map {
+public:
+    Map(int width, int height) : width_(width), height_(height) {
+        // Initialize the map with empty villages
+        map_.resize(width * height);
+    }
+
+    int width() const { return width_; }
+    int height() const { return height_; }
+
+    Village& findVillage(int x, int y) {
+        return map_[y * width_ + x];
+    }
+
+    const Village& findVillage(int x, int y) const {
+        return map_[y * width_ + x];
+    }
+
+private:
+    int width_;
+    int height_;
+    std::vector<Village> map_;
+};
